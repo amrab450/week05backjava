7656ceb5d59b281dc9c03e5cce90d2d40b7e3838
amrab450 <abmrabouli@gmail.com>
2024-12-15 14:42:20 -0500

    Update AsteriskLogger.java

--- week05backjava/src/week05backjava/AsteriskLogger.java
+++ week05backjava/src/week05backjava/AsteriskLogger.java
@@ -1,14 +1,18 @@
 package week05backjava;
 
+
+//Implement the AsteriskLogger class
 public class AsteriskLogger implements Logger {
    
     public void log(String message) {
-        System.out.println("***" + message + "***");
+    	// Print the message with 3 asterisks on either side
+    	System.out.println("***" + message + "***");
     }
 
    
     public void error(String message) {
-        System.out.println("****************");
+    	 // Print the error message inside a box of asterisks
+    	System.out.println("****************");
         System.out.println("***ERROR: " + message + "***");
         System.out.println("****************");
     }
