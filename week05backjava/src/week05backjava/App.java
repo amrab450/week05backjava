c253743ce6bb4ec4bbca0ca17e0f70339e2d0d5d
amrab450 <abmrabouli@gmail.com>
2024-12-15 14:41:20 -0500

    Update App.java

--- week05backjava/src/week05backjava/App.java
+++ week05backjava/src/week05backjava/App.java
@@ -1,10 +1,16 @@
-package week05backjava;
+ackage week05backjava;
 
+
+//Create the App class with the main method
 public class App {
     public static void main(String[] args) {
-        Logger asteriskLogger = new AsteriskLogger();
+    	// Instantiate instances of each logger class
+    	
+    	Logger asteriskLogger = new AsteriskLogger();
         Logger spacedLogger = new SpacedLogger();
-
+       
+        // Test both methods on both instances
+       
         asteriskLogger.log("Hello");
         asteriskLogger.error("Hello");
 
