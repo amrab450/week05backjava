6dff5b0bd384b0d2e7a9ab4f0e1e370a800d5a46
amrab450 <abmrabouli@gmail.com>
2024-12-15 14:44:14 -0500

    Update SpacedLogger.java

--- week05backjava/src/week05backjava/SpacedLogger.java
+++ week05backjava/src/week05backjava/SpacedLogger.java
@@ -1,9 +1,12 @@
 package week05backjava;
 
+
+  //Implement the SpacedLogger class
 public class SpacedLogger implements Logger {
     
     public void log(String message) {
-        for (char c : message.toCharArray()) {
+    	  // Print each character of the message with a space
+    	for (char c : message.toCharArray()) {
             System.out.print(c + " ");
         }
         System.out.println();
@@ -11,9 +14,11 @@ public class SpacedLogger implements Logger {
 
    
     public void error(String message) {
-        System.out.print("ERROR: ");
+    	// Print the error message with each character spaced out
+    	System.out.print("ERROR: ");
         for (char c : message.toCharArray()) {
-            System.out.print(c + " ");
+            
+        	System.out.print(c + " ");
         }
         System.out.println();
     }
